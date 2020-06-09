6931430200163604b53993c727406d3e9a29de3f
Meshkatul Jannat <jannat.m.bd@gmail.com>
2020-06-08 23:27:15 -0500

    first commit

---
+++ src/test/java/org/perscholas/bakeryshop/BakeryshopApplicationTests.java
@@ -0,0 +1,13 @@
+package org.perscholas.bakeryshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BakeryshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
